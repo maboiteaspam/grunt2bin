d8aeadae6956ff5b366d3071b8b3d763e6d5d2c2
clement <maboiteaspam@wanadoo.fr>
2015-08-19 22:27:25 +0800

    let s try like this for instance. It give s support or dual gruntfile configuration loading at runtime.

--- index.js
+++ index.js
@@ -1,30 +1,71 @@
 var path = require('path')
 var fs = require('fs')
+var osenv = require('osenv')
 
 module.exports = function (program) {
 
   var cwd = process.cwd()
 
   var caller = getCallerLocation()
+  var moduleLocation = findPackageFileAlongPath(caller)
 
   var grunt = require('grunt')
 
   var mainGruntfile = path.join(__dirname, 'Gruntfile.js');
 
-  grunt.file.setBase(caller)
+  var userGruntfileName = null
+
+  var userGrunt = null
   var userGruntfile = null
   grunt.setUserGruntfile = function(u){
+    userGruntfileName = u
     userGruntfile = path.join(cwd, u)
-  }
-  program(grunt, cwd)
-
-  if (userGruntfile && !fs.existsSync(userGruntfile)) {
-    userGruntfile = mainGruntfile
+    if (fs.existsSync(userGruntfile)) {
+      userGrunt = require(userGruntfile)
+    }
   }
 
+  var systemUserGrunt = null
+  var systemUserGruntfile = null
+  grunt.loadSystemUserGruntfile = function (what) {
+    // can/must be called only from userGruntfile.
+    what = what || 'config run'
+    if (userGruntfileName) {
+      systemUserGruntfile = path.join(osenv.home(), userGruntfileName, 'index.js')
+      if (fs.existsSync(systemUserGruntfile)) {
+        systemUserGrunt = require(systemUserGruntfile)
+        if (what.match(/config/)) {
+          systemUserGrunt && grunt.file.setBase(path.dirname(systemUserGruntfile))
+          systemUserGrunt && systemUserGrunt.config(grunt, cwd)
+          grunt.file.setBase(userGruntfile)
+        }
+        if (what.match(/run/)) {
+          systemUserGrunt && grunt.file.setBase(path.dirname(systemUserGruntfile))
+          systemUserGrunt && systemUserGrunt.run(grunt, cwd)
+          grunt.file.setBase(userGruntfile)
+        }
+      }
+    }
+  }
+
+  grunt.file.setBase(moduleLocation || caller)
+  program.config(grunt, cwd)
+
+  userGrunt && grunt.file.setBase(path.dirname(userGruntfile))
+  userGrunt && userGrunt.config(grunt, cwd)
+
+  grunt.file.setBase(moduleLocation || caller)
+  program.run(grunt, cwd)
+
+  userGrunt && grunt.file.setBase(path.dirname(userGruntfile))
+  userGrunt && userGrunt.run(grunt, cwd)
+
+  grunt.file.setBase(moduleLocation || caller)
   grunt.tasks('default', {
-    gruntfile: userGruntfile,
-    base: cwd
+    gruntfile: mainGruntfile,
+    base: cwd,
+    verbose: !!process.argv.join(' ').match(/--verbose/),
+    debug: !!process.argv.join(' ').match(/--debug/)
   })
 }
 
@@ -43,4 +84,27 @@ function getCallerLocation () {
   caller = path.dirname(caller[1])
 
   return caller;
+}
+
+function findPackageFileAlongPath (p) {
+
+  var found = false
+  do{
+    if (fs.existsSync(path.join(p, 'package.json'))) {
+      var pk = require(path.join(p, 'package.json'))
+      if (pk.bin) {
+        Object.keys(pk.bin).forEach(function(name){
+          var f = pk.bin[name]
+          if (!found && fs.existsSync(path.join(p,f))) {
+            found = true
+          }
+        })
+      }
+    }
+    if (!found&& p.length>1) {
+      p = path.resolve(p, '..')
+    }
+  }while(found);
+
+  return p || found;
 }
